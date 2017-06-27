5e8c552c07f02568dc4fa4e9e223b5b25e14e6c8
Kaung <Kaung@DESKTOP-8A4B4QP>
2017-06-27 22:47:17 +0630

    Initial

---
+++ kotoba-api/src/main/java/com/kaungyeehein/kotoba/api/HelloController.java
@@ -0,0 +1,15 @@
+package com.kaungyeehein.kotoba.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello() {
+		return "welcome";
+	}
+
+}
---
+++ kotoba-api/src/main/java/com/kaungyeehein/kotoba/api/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.kaungyeehein.kotoba.api.service;
+
+import java.util.List;
+
+import com.kaungyeehein.kotoba.api.domain.Person;
+
+public interface HelloService {
+
+	public List<Person> getPerson();
+}
