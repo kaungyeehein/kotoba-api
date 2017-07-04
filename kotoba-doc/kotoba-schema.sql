d4eae56b1ca65ee2d313a178a0d003586e18d4ec
kaungyeehein <kaungyeehein@gmail.com>
2017-07-04 09:09:49 +0630

    Add doc and create schema

--- kotoba-doc/kotoba-schema.sql
+++ kotoba-doc/kotoba-schema.sql
@@ -3,23 +3,55 @@ DROP SCHEMA IF EXISTS `kotoba` ;
 CREATE SCHEMA `kotoba` ;
 USE `kotoba` ;
 
-CREATE TABLE `person` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `gender` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `kotoba` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `kana` VARCHAR(45) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `romaji` VARCHAR(100) NOT NULL,
+  `kanji` VARCHAR(45) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `level` VARCHAR(2) NOT NULL,
+  `chapter` TINYINT(3) NULL DEFAULT NULL,
+  `type` VARCHAR(10) NOT NULL,
+  `sample` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `create` TIMESTAMP NOT NULL,
+  `update` TIMESTAMP NOT NULL,
+  `delete` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `KOTOBA_KANA` (`kana` ASC),
+  INDEX `KOTOBA_ROMAJI` (`romaji` ASC),
+  INDEX `KOTOBA_KANJI` (`kanji` ASC));
   
-CREATE TABLE `pet` (
-  `pet_id` INT NOT NULL,
-  `owner_id` INT NULL,
-  `pet_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`pet_id`));
+CREATE TABLE `kotoba_mm` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(100) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `sample` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `delete` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`id`, `name`),
+  INDEX `KOTOBA_MM_NAME` (`name` ASC),
+  CONSTRAINT `KOTOBA_MM_ID`
+    FOREIGN KEY (`id`)
+    REFERENCES `kotoba` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE);
 
-INSERT INTO `person` (`id`, `name`, `gender`, `email`) VALUES (1, 'Kaung', 'Male', 'kaung@gmail.com');
-INSERT INTO `person` (`id`, `name`, `gender`, `email`) VALUES (2, 'Zaw', 'Male', 'zaw@gmail.com');
-INSERT INTO `person` (`id`, `name`, `gender`, `email`) VALUES (3, 'Aye', 'Female', 'aye@gmail.com');
+CREATE TABLE `kotoba_en` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `sample` VARCHAR(255) NULL DEFAULT NULL,
+  `delete` TINYINT	(1) NOT NULL,
+  PRIMARY KEY (`id`, `name`),
+  INDEX `KOTOBA_EN_NAME` (`name` ASC),
+  CONSTRAINT `KOTOBA_EN_ID`
+    FOREIGN KEY (`id`)
+    REFERENCES `kotoba` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE);
+    
+INSERT INTO `kotoba` (`kana`, `romaji`, `kanji`, `level`, `chapter`, `type`, `sample`, `delete`) VALUES ('くるま', 'kuruma', '車', 'N5', '1', 'n', 'これは車です。', '0');
+INSERT INTO `kotoba` (`kana`, `romaji`, `kanji`, `level`, `chapter`, `type`, `sample`, `delete`) VALUES ('たべる', 'taberu', '食べる', 'N5', '4', 'v2', 'ひるごはんを食べます。', '0');
 
-INSERT INTO `pet` (`pet_id`, `owner_id`, `pet_name`) VALUES (1, 1, 'dog');
-INSERT INTO `pet` (`pet_id`, `owner_id`, `pet_name`) VALUES (2, 1, 'bird');
-INSERT INTO `pet` (`pet_id`, `owner_id`, `pet_name`) VALUES (3, 3, 'cat');
+INSERT INTO `kotoba_en` (`id`, `name`, `sample`, `delete`) VALUES ('1', 'car', 'This is a car.', '0');
+INSERT INTO `kotoba_en` (`id`, `name`, `sample`, `delete`) VALUES ('1', 'vehicle', 'This is a vehicle.', '0');
+INSERT INTO `kotoba_en` (`id`, `name`, `sample`, `delete`) VALUES ('2', 'eat', 'I eat lunch.', '0');
+
+INSERT INTO `kotoba_mm` (`id`, `name`, `sample`, `delete`) VALUES ('1', 'ကား', 'သည်ဟာသည် ကား ဖြစ်သည်။', '0');
+INSERT INTO `kotoba_mm` (`id`, `name`, `sample`, `delete`) VALUES ('2', 'စားသည်', 'နေ့လည်စာ စားသည်။', '0');
